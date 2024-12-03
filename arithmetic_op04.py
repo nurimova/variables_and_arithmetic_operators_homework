cf3745cef8c765f6fab55b4f83bb735c929a5401
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-03 20:33:33 +0200

    Update arithmetic_op04.py

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,5 +1,7 @@
-# Create a variable "a"  and assign it the value of the int 5.
+# a o'zgaruvchisini yarating va unga 8 butun son qiymatini bering
 
-# Create a variable "b"  and assign it the value of the int 4.
+# b o'zgaruvchisini yarating va unga 3 butun son qiymatini bering
 
-# Create a variable called 'answer' and assign it the multiplication of a and b.
+# c o'zgaruvchisini yarating va unga 5 butun son qiymatini bering
+
+# total o'zgaruvchisini yarating va total = a + b/c ni hisoblang
