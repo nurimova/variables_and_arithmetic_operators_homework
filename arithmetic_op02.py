fb223e7ad08df2f9237443498af907c5c2f77413
nurimova <n.b.sitora@gmail.com>
2024-12-06 14:39:14 +0500

    arithmetic

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,8 +1,9 @@
 # a o'zgaruvchisini yarating va unga 3 butun son qiymatini bering
-
+a=3
 # b o'zgaruvchisini yarating va unga 4 butun son qiymatini bering
-
+b=4
 # c o'zgaruvchisini yarating va unga 5 butun son qiymatini bering
-
+c=5
 # answer o'zgaruvchisini yarating va a, b va c ning ayirmasini hisoblang
-
+answer=3-4-5
+print(answer)
