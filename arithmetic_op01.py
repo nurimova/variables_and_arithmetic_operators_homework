896dfd6aad8931905704619c7862747ac320c326
nurimova <n.b.sitora@gmail.com>
2024-12-06 14:27:51 +0500

    arifmetik 1

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,9 @@
 # a degan o'zgaruvchini yarating va unga 12 qiymatini bering.
-
+a = 12
 # b degan o'zgaruvchini yarating va unga 53 qiymatini bering.
-
+b = 53
 # c degan o'zgaruvchini yarating va unga 26 qiymatini bering.
-
+c = 26
 #  total degan o'zgaruvchini yarating va a, b va c qiymatlaring yig'indisini hisoblang.
+total = a + b + c
+print(total)
