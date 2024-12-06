549c627d4e202a0e3bdb851b3e07fc706b55e307
nurimova <n.b.sitora@gmail.com>
2024-12-06 15:40:19 +0500

    arithmetic 3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,7 +1,9 @@
 # a o'zgaruvchisini yarating va unga 7 butun son qiymatini bering
-
+a=7
 # b o'zgaruvchisini yarating va unga 5 butun son qiymatini bering
-
+b=5
 # c o'zgaruvchisini yarating va unga 9 butun son qiymatini bering
-
+c=9
 # total o'zgaruvchisini yarating va total = (a + b) * c ni hisoblang.
+total=(a+b)*c
+print(total)
