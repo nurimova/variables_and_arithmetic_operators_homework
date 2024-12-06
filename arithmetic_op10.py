1f5062cc1f0a0fff023a926e9b64f5bd77a54978
nurimova <n.b.sitora@gmail.com>
2024-12-06 22:36:45 +0500

    arithmetic 10

--- arithmetic_op10.py
+++ arithmetic_op10.py
@@ -1,5 +1,6 @@
 # n o'zgaruvchisini yarating va unga 4 qiymatini bering
-
+n=4
 # total o'zgaruvchisini yarating va unga ifodaning qiymatini bering: 2^n
-
+total=2**n
 # Natijani chop eting
+print(total)
