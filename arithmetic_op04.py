30880f83ead5136caade54db706557ac926d0bd6
nurimova <n.b.sitora@gmail.com>
2024-12-06 15:42:55 +0500

    arithmetic 4

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,7 +1,9 @@
 # a o'zgaruvchisini yarating va unga 8 butun son qiymatini bering
-
+a=8
 # b o'zgaruvchisini yarating va unga 3 butun son qiymatini bering
-
+b=3
 # c o'zgaruvchisini yarating va unga 5 butun son qiymatini bering
-
+c=5
 # total o'zgaruvchisini yarating va total = a + b/c ni hisoblang
+total=a+b/c
+print(total)
