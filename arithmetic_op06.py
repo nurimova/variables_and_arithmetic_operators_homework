758e8378d444578fd70ddb0c45e2b89bccede7f1
nurimova <n.b.sitora@gmail.com>
2024-12-06 22:00:05 +0500

    arithmetic 6

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,7 +1,9 @@
 # a o'zgaruvchisini yarating va unga 11 butun son qiymatini bering
-
+a=11
 # b o'zgaruvchisini yarating va unga 7 butun son qiymatini bering
-
+b=7
 # c o'zgaruvchisini yarating va unga 12 butun son qiymatini bering
-
+c=12
 # total o'zgaruvchisini yarating va total = c + b//a ni hisoblang
+total=c+b//a
+print(total)
