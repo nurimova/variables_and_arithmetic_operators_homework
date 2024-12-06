c21b6610ea2be86d1ce8aba8ea24f8f400a44725
nurimova <n.b.sitora@gmail.com>
2024-12-06 22:27:07 +0500

    arithmetic 7

--- arithmetic_op07.py
+++ arithmetic_op07.py
@@ -1,7 +1,9 @@
 # a o'zgaruvchisini yarating va unga 2 butun son qiymatini bering
-
+a=2
 # b o'zgaruvchisini yarating va unga 10 butun son qiymatini bering
-
+b=10
 # c o'zgaruvchisini yarating va unga 10 butun son qiymatini bering
-
+c=10
 # total o'zgaruvchisini yarating va total = (b**a)//10 ni hisoblang
+total=(b**a)//10
+print(total)
