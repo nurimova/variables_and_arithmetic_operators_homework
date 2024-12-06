936c630c6c950b990e9e766dd3c83f0d7b357edb
nurimova <n.b.sitora@gmail.com>
2024-12-06 22:35:01 +0500

    arithmetic 9

--- arithmetic_op09.py
+++ arithmetic_op09.py
@@ -1,9 +1,11 @@
 # "a" o'zgaruvchisini yarating va unga 8 butun son qiymatini bering
-
+a=8
 # "b" o'zgaruvchisini yarating va unga 3 butun son qiymatini bering
-
+b=3
 # "c" o'zgaruvchisini yarating va unga 2 butun son qiymatini bering
-
+c=2
 # "d" o'zgaruvchisini yarating va unga 4 butun son qiymatini bering
-
+d=4
 # "total" o'zgaruvchisini yarating va unga ifodaning qiymatini bering: a + b - c * d (answer = a + b - c * d) ni hisoblang
+total=a+b-c*d
+print(total)
